9763c485ff66e1177b5a8c9a2b8f33680d42c062
Susmoy Debnath <susmoydebnath05@gmail.com>
2023-07-14 15:09:34 +0600

    Updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# Back-End-Developer-Capstone
+META Back-End Developer Course Capstone Project implemented by Susmoy Debnath
+
+Please, see Readme.txt file for review process in Week 4 of the course.
+
---
+++ littlelemon/littlelemon/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for littlelemon project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+<<<<<<< HEAD
+For more information on this file, see
+=======
+For more information on this file, see.
+>>>>>>> origin/master
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'littlelemon.settings')
+
+application = get_asgi_application()
